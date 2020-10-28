4dc88b7fee8e70f6d78a28ab40d91b5e6d8b54ff
Damien de la Bruère-Terreault <damiendlbt@gmail.com>
2020-10-28 16:32:08 -0400

    Refafctor de controlle

--- src/cegepst/Player.java
+++ src/cegepst/Player.java
@@ -1,14 +1,15 @@
 package cegepst;
 
 import cegepst.engine.Buffer;
+import cegepst.engine.controls.MovementController;
 import cegepst.engine.entity.ControllableEntity;
 
 import java.awt.*;
 
 public class Player extends ControllableEntity {
 
-    public Player(InputHandler inputHandler) {
-        super(inputHandler);
+    public Player(GameController controller) {
+        super(controller);
         super.setDimension(30,30);
         super.teleport(100,100);
         super.setSpeed(4);
